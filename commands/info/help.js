4bca54a8cc627d3b7e6bc349843502ab8c737774
Follin <93303026+Skyee06@users.noreply.github.com>
2022-02-02 20:32:32 +0700

    Create help.js

---
+++ commands/info/help.js
@@ -0,0 +1,44 @@
+const Discord = require("discord.js");
+const { MessageEmbed } = require("discord.js");
+const { Color, Prefix } = require("../../config.js");
+
+module.exports = {
+  name: "help",
+  aliases: ["h"],
+  description: "Help Command!",
+  usage: "Help | <Command Name>",
+  run: async(client, message, args) => {
+    
+    message.delete();
+    
+    let embed = new MessageEmbed()
+    .setColor(Color)
+    .setTitle(`${client.user.username} Commands!`)
+    .setDescription(`Use ${Prefix}Help <Command Name> For More Command Information!`)
+    .addField("😜 Fun", " `ascii`, `avatar`, `coinflip`, `hack`, `howgay`, `meme`, `rate`, `randomnumber`")
+    .addField("ℹ️ Info", " `help`, `ping`, `serverinfo`, `userinfo`, `weather`")
+    .addField("⚒️ Moderation", " `ban`, `clear`, `kick`, `mute`, `unban`, `unmute`, `warn`, `warnings`")
+    .setFooter(`Requested By ${message.author.username}`)
+    .setTimestamp();
+    
+    if (!args.length) return message.channel.send(embed);
+
+    let cmd =
+      client.commands.get(args[0].toLowerCase()) ||
+      client.commands.get(client.aliases.get(args[0].toLowerCase()));
+
+    let embed2 = new MessageEmbed()
+      .setColor(Color)
+      .setTitle(`${cmd.name} Information!`)
+      .addField(`Aliases`, cmd.aliases || "None!")
+      .addField(`Usage`, cmd.usage || "No Usage")
+      .addField(`Description`, cmd.description || "No Description!")
+      .setTimestamp();
+
+    if (cmd) {
+      return message.channel.send(embed2);
+    } else {
+      return message.channel.send(embed);
+    }
+  }
+};
